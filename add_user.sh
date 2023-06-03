ab016a8ca93fd6b231649da06596334ad097e6b5
vilija19 <60488526+vilija19@users.noreply.github.com>
2023-06-03 14:29:52 +0300

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -50,9 +50,9 @@ fi
 
 sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh/
 
-if [ current_user == 'root' ]; then
-	sudo echo "$NEW_USER  ALL=NOPASSWD: ALL" >> /etc/sudoers
-	  echo "Sudo without password enabled"
+if [ current_user == "root" ]; then
+	echo "$NEW_USER  ALL=NOPASSWD: ALL" | sudo tee /etc/sudoers
+	echo "Sudo without password enabled"
 fi
 
 echo "User $NEW_USER added"
